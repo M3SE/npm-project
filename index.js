03a26495de9099afb462d2812b21b75e1187822e
M3SE <simonc691@gmail.com>
2024-07-02 15:18:17 -0400

    finished assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+// this imports the moment module
+const moment = require('moment');
+
+// Placeholder for npm start
+console.log("this is a placeholder text");
+
+// Using moment creating timestamp for response arrival time
+console.log("Response Timestamp: ", moment().format('h:mm:ss a'));
